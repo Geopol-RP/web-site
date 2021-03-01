b39bcf7a7b4578e856e0bf5868b0b943927dca46
Dany01000110 <danette41240mc@gmail.com>
2021-03-01 11:07:26 +0100

    Edit .htaccess file - RewriteRule for panel link -> Fix 1

--- panel.php
+++ panel.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 if (!isset($_SESSION['auth']['user'])) {
-    header("location: login");
+    header("location: https://geopol-rp.fr/panel/index");
     exit();
 }
 ?>

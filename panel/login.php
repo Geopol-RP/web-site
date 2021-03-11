b28ddee89fc86a30fe15af7e3efaf8fd0ace9a48
Dany01000110 <danette41240mc@gmail.com>
2021-03-11 21:49:51 +0100

    Redesign of the connection panel

--- panel/login.php
+++ panel/login.php
@@ -41,12 +41,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Géopol-Rp | Panel - Connexion</title>
+    <title>Géopol-RP | Panel - Connexion</title>
     <link rel="stylesheet" href="css/login.css">
 
     <meta name="theme-color" content="#44d8e8"> 
     <meta name="msapplication-navbutton-color" content="#44d8e8"> 
     <meta name="apple-mobile-web-app-status-bar-style" content="#44d8e8">
+
+    <!-- Favicons -->
+
+    <link rel="shortcut icon" type="image/png" href="../img/favicons/favicon_geopol_16x16.png" sizes="16x16">
+    <link rel="shortcut icon" type="image/png" href="../img/favicons/favicon_geopol_32x32.png" sizes="32x32">
+    <link rel="shrotcut icon" type="image/png" href="../img/favicons/favicon_geopol_android_google_chrome_192x192.png" sizes="192x192">
+    <link rel="shortcut icon" href="../img/favicons/favicon_geopol_32x32.ico" type="image/x-icon">
 
     <!-- SEO -->
 
@@ -65,37 +72,42 @@
     <meta property="twitter:image" content="https://www.geopol-rp.fr/img/image_embed.webp" />
 </head>
 <body>
-    <div class="container-login">
-        <form action="?loginAction" method="POST">
-                <h1 id="title-login">Connexion</h1>
+    <div class="container">
+        <div class="background"></div>
+        <div class="content">
+            <div class="container-login">
+                <form action="?loginAction" method="POST">
+                    <h1 id="title-login">Connexion</h1>
                 
-                <label><b>Nom d'utilisateur</b></label>
-                <br>
-                <input type="text" placeholder="Entrer votre nom d'utilisateur" name="username" required>
-                <br>
-                <label><b>Mot de passe</b></label>
-                <br>
-                <input type="password" placeholder="Entrer votre mot de passe" name="password" id="password" required>
-                <br>
-                <label for="checkbox" id="label-checkbox">
-                    <input type="checkbox" id="checkbox">
-                    Afficher le mot de passe
-                </label>
-                <br>    
-                <input type="submit" id='submit' value='CONNEXION' >
-                <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2) {
-                        echo "<p align='center' style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                    <label><b>Nom d'utilisateur</b></label>
+                    <br>
+                    <input type="text" placeholder="Entrer votre nom d'utilisateur" name="username" required>
+                    <br>
+                    <label><b>Mot de passe</b></label>
+                    <br>
+                    <input type="password" placeholder="Entrer votre mot de passe" name="password" id="password" required>
+                    <br>
+                    <label for="checkbox" id="label-checkbox">
+                        <input type="checkbox" id="checkbox">
+                        Afficher le mot de passe
+                    </label>
+                    <br>    
+                    <input type="submit" id='submit' value='CONNEXION' >
+                    <?php
+                    if(isset($_GET['erreur'])){
+                        $err = $_GET['erreur'];
+                        if($err==1 || $err==2) {
+                            echo "<p align='center' style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                        }
+                        if($err==3) {
+                            echo "<p align='center' style='color:red'>Vous devez être connecté pour accéder à l'intra d'<b>Étudia Sphère</b> !</p>";
+                        }
                     }
-                    if($err==3) {
-                        echo "<p align='center' style='color:red'>Vous devez être connecté pour accéder à l'intra d'<b>Étudia Sphère</b> !</p>";
-                    }
-                }
-                ?>
-        </form>
-        <p id="copy">Géopol-RP &copy <?= date("Y") ?></p>
+                    ?>
+                </form>
+                <p id="copy">Géopol-RP &copy <?= date("Y") ?></p>
+            </div>
+        </div>
     </div>
     <a href="../" class="returnHomePage"><i class="fas fa-undo-alt"></i> Retourner à la page d'accueil</a>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
